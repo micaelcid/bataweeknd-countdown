4e6198ae5b0defe1b3b59fdce667518ab343fe69
Micael Cid <micaelcid10@gmail.com>
2021-10-17 17:21:23 -0300

    Add fireworks during bataweeknd and also declare when its over

--- src/js/countdown/countdown.js
+++ src/js/countdown/countdown.js
@@ -1,18 +1,17 @@
-let daysElement = document.getElementById("days")
-let daysLabelElement = document.getElementById("days-label")
-let hoursElement = document.getElementById("hours")
-let hoursLabelElement = document.getElementById("hours-label")
-let minutesElement = document.getElementById("minutes")
-let minutesLabelElement = document.getElementById("minutes-label")
-let secondsElement = document.getElementById("seconds")
-let secondsLabelElement = document.getElementById("seconds-label")
+const bataweekndStartDate = new Date('2021-10-22 22:00:00')
+const bataweekndEndDate = new Date('2021-10-31 14:00:00')
 
-const bataweekndDate = new Date('2021-10-22 22:00:00')
+const buildLabel = (value, label) => value > 1 ? `${label}s` : label
+
+const renderCountdownItem = (item, value, label) => {
+    document.getElementById(`${item}`).innerHTML = value 
+    document.getElementById(`${item}-label`).innerHTML = buildLabel(value, label)
+}
 
 const renderCountdown = () => {
     const nowDate = new Date()
 
-    let delta = Math.abs(bataweekndDate - nowDate) / 1000;
+    let delta = Math.abs(bataweekndStartDate - nowDate) / 1000;
 
     const days = Math.floor(delta / 86400);
     delta -= days * 86400;
@@ -24,15 +23,20 @@ const renderCountdown = () => {
     delta -= minutes * 60;
 
     const seconds = Math.floor(delta % 60);
+
+    const isBataweekndHappening = bataweekndEndDate - nowDate > -1000;
     
-    daysElement.innerHTML = days 
-    daysLabelElement.innerHTML = days > 1 ? 'Days' : 'Day'
-    hoursElement.innerHTML = hours
-    hoursLabelElement.innerHTML = hours > 1 ? 'Hours' : 'Hour'
-    minutesElement.innerHTML = minutes
-    minutesLabelElement.innerHTML = minutes > 1 ? 'Minutes' : 'Minute'
-    secondsElement.innerHTML = seconds
-    secondsLabelElement.innerHTML = seconds > 1 ? 'Seconds' : 'Second'
+    if (isBataweekndHappening) {
+        renderCountdownItem('days', days, 'Day')
+        renderCountdownItem('hours', hours, 'Hour')
+        renderCountdownItem('minutes', minutes, 'Minute')
+        renderCountdownItem('seconds', seconds, 'Second')
+    } else {
+        renderCountdownItem('days', 'B', 'O')
+        renderCountdownItem('hours', 'A', 'V')
+        renderCountdownItem('minutes', 'T', 'E')
+        renderCountdownItem('seconds', 'A', 'R')
+    }
 }
 
 setInterval(renderCountdown, 1000)
